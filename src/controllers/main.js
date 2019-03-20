512f678a13e3325fbc183f6aaad9a033d3a1e483
denisogolubev <denisogolubev@gmail.com>
2019-03-21 01:36:52 +0200

    add br at textarea after keypress 13(Enter)

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -286,6 +286,23 @@ var UIController = function () {
             
         },
 
+        insertTextAtCursor: function(el, text, offset) {
+            var val = el.value, endIndex, range, doc = el.ownerDocument;
+            if (typeof el.selectionStart == "number"
+                    && typeof el.selectionEnd == "number") {
+                endIndex = el.selectionEnd;
+                el.value = val.slice(0, endIndex) + text + val.slice(endIndex);
+                el.selectionStart = el.selectionEnd = endIndex + text.length+(offset?offset:0);
+            } else if (doc.selection != "undefined" && doc.selection.createRange) {
+                el.focus();
+                range = doc.selection.createRange();
+                range.collapse(false);
+                range.text = text;
+                range.select();
+            }
+        },
+
+
         clearFields: function() {
             var fields, fieldsArray;
 
@@ -348,14 +365,14 @@ var combiController = (function (editCtrl, UICtrl) {
 
             document.getElementById(DOM.linksButtonMoreDet).addEventListener('click', ctrlAddLinksItem)
 
+            document.querySelector(DOM.textInput).addEventListener('keypress', function(event) {
+                var el = document.querySelector(DOM.textInput);
+                if (event.keyCode === 13) {
+                    UIController.insertTextAtCursor(el, '<br>');
+                }
+            })
 
-            
-            
 
-            
-
-
-            
             };
             
         
